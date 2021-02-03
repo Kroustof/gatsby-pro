93d2d3b6ed4876b6e7c546ec508fe7a809d32dd2
Kroustof <didier.billars@hotmail.fr>
2021-02-03 11:16:06 +0400

    nouveau test form

--- src/components/mailchimpForm.jsx
+++ src/components/mailchimpForm.jsx
@@ -2,14 +2,14 @@ import React from "react"
 
 const MailchimpForm = () => {
   return (
-    <form form-name="mailchimp" method="POST" data-netlify="true" netlify-honeypot='bot-field'>
+    <form name="mailchimp" method="POST" data-netlify="true" data-netlify-honeypot='bot-field'>
       <p>
         <label>
           Your Email: <input type="email" name="email" />
         </label>
       </p>
-      <input type="hidden" name="form-name" value="contact"/>
-      <input type="hidden" name="bot-field"/>
+      <input type="hidden" form-name="form-name" value="contact"/>
+      <input type="hidden" form-name="bot-field"/>
       <p>
         <button type="submit">Send</button>
       </p>
